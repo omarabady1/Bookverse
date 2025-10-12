060403b08b8286be1724ba8ba1ca4defd58e4a69
omarabady <omarabady44@gmail.com>
2025-10-12 10:04:47 +0300

    Refactor Book Details View Sections

---
+++ lib/features/home/presentation/views/widgets/book_details_section.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/utils/styles.dart';
+import 'best_seller_list_item.dart';
+import 'custom_button.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset('assets/images/book_cover.png'),
+        SizedBox(height: 43),
+        Text('The Jungle Book', style: Styles.sectra20.copyWith(fontSize: 30)),
+        SizedBox(height: 6),
+        Text(
+          'Rudyard Kipling',
+          style: Styles.title18.copyWith(color: Colors.grey),
+        ),
+        SizedBox(height: 16),
+        BookRating(),
+        SizedBox(height: 37),
+        CustomButton(),
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,7 @@
 import 'package:bookly/features/home/presentation/views/widgets/book_details_custom_app_bar.dart';
-import 'package:bookly/features/home/presentation/views/widgets/recommended_books_list.dart';
+import 'package:bookly/features/home/presentation/views/widgets/recommended_books_section.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/styles.dart';
-import 'best_seller_list_item.dart';
-import 'custom_button.dart';
+import 'book_details_section.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -16,34 +13,9 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const BookDetailsCustomAppBar(),
           SizedBox(height: 32),
-          Image.asset('assets/images/book_cover.png'),
-          SizedBox(height: 43),
-          Text(
-            'The Jungle Book',
-            style: Styles.sectra20.copyWith(fontSize: 30),
-          ),
-          SizedBox(height: 6),
-          Text(
-            'Rudyard Kipling',
-            style: Styles.title18.copyWith(color: Colors.grey),
-          ),
-          SizedBox(height: 16),
-          BookRating(),
-          SizedBox(height: 37),
-          CustomButton(),
+          BookDetailsSection(),
           SizedBox(height: 70),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 24.0),
-              child: Text(
-                'You can also like',
-                style: Styles.title18.copyWith(fontSize: 14),
-              ),
-            ),
-          ),
-          SizedBox(height: 16),
-          RecommendedBooksList(),
+          RecommendedBooksSection(),
         ],
       ),
     );
