5ba8b09c6d6cf2241c413d0f2f96d3875e76a27f
omarabady <omarabady44@gmail.com>
2025-10-14 20:10:23 +0300

    I'm  tired, boss

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -26,7 +26,7 @@ class HomeRepoImpl implements HomeRepo {
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
+
     throw UnimplementedError();
   }
 }
