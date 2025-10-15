baf632cc8a4f090b0a399b5dc603514200d6a3a9
omarabady <omarabady44@gmail.com>
2025-10-15 14:48:25 +0300

    Newest Books Cubit + States Done

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model.dart';
+import 'package:meta/meta.dart';
+import '../../../data/repos/home_repo.dart';
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}
