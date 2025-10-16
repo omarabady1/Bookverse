3bd414ad64d34f24fa8db06bc384c0fbc4c6fcae
omarabady <omarabady44@gmail.com>
2025-10-16 11:46:07 +0300

    Setup Service Locator + Provide Cubits

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books__cubit.dart';
+import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'constants.dart';
 
 void main() {
+  setupServiceLoctor();
   runApp(const Bookverse());
 }
 
@@ -11,10 +17,16 @@ class Bookverse extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>())),
+        BlocProvider(create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>())),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      ),
     );
   }
 }
