edb14488c19fd149f28103cba288081dae97d9c3
omarabady <omarabady44@gmail.com>
2025-10-18 21:22:07 +0300

    Add the Url Launch Package and Configure for Android and IOS

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -12,7 +12,7 @@ class HomeRepoImpl implements HomeRepo {
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
     try {
-      var data = await apiService.get('volumes?q=subject:general&orderBy=newest');
+      var data = await apiService.get('volumes?q=subject:science&orderBy=newest');
       List<BookModel> bookList = [];
       for (var item in data['items']) {
         bookList.add(BookModel.fromJson(item));
