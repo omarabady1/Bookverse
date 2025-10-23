ca3153aea3fe7cad2eb0ff6bb6cdbbc66f8b365d
omarabady <omarabady44@gmail.com>
2025-10-23 09:27:52 +0300

    Issue Fixed: Api Quota Exceeded

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -2,12 +2,13 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   final String _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final _apiKey = 'AIzaSyA89hBcOejrJY1hcTSPgUdy3qnQoloplgE';
   final Dio dio;
 
   ApiService(this.dio);
 
   Future<Map<String, dynamic>> get(String endPoint) async {
-    var response = await dio.get(_baseUrl + endPoint);
+    var response = await dio.get('$_baseUrl$endPoint&key=$_apiKey');
     return response.data;
   }
 }
